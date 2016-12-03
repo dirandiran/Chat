5d95182befe43d3e4df2ccbec1e51e0cf1af52af
Pavel Klevakin <pavel.klevakin.05.1990@gmail.com>
2016-12-03 17:38:04 +0500

    Добавлены классы с сообщением и DTO. Начата реализация хранилища сообщений

---
+++ src/main/java/chat/Message.java
@@ -0,0 +1,28 @@
+package chat;
+
+import java.util.Date;
+
+/**
+ * Created by pavel on 03.12.16.
+ */
+public class Message {
+    private String message;
+
+    private Date messageData;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Date getMessageData() {
+        return messageData;
+    }
+
+    public void setMessageData(Date messageData) {
+        this.messageData = messageData;
+    }
+}
